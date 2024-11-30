adf9b26abc22c1982afb45d57c761d02b7b1c2f6
meladsharafi <mx10100@gmail.com>
2024-11-30 11:06:54 +0330

    1403/09/10

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [   
+    "./assets/**/*.*",       
+    "./index.html",       
+    ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
